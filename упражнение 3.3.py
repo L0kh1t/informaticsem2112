459c9cdb327dd8ef2e12beea28cce62f58dcd3cc
L0kh1t <sxtandrey@gmail.com>
2023-09-22 18:10:56 +0400

    упражнение 3.3

--- упражнение 3.3.py
+++ упражнение 3.3.py
@@ -1,15 +1,11 @@
-def f(a, b):
-    m = 0
+N = list(map(int, input().split()))
+a = N[0]
+b = N[1]
+m = a
+n = b
+while a != 0 and b != 0:
     if a > b:
-        m = a
-    if b > a:
-        m = b
-    while(True):
-        if (m % a == 0) and (m % b == 0):
-            d = m
-            break
-        m += 1
-    return d
-
-result = str(f(a, b)//a) + ' ' + str(f(a, b)//b) + ' ' + str(f(a, b))
-print(result)
+        a = a % b
+    else:
+        b = b % a
+print(str(a+b)+" " + str((m/(a+b))) + " " + str(n/(a+b)))
